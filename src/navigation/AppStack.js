b048fecc012dc75b51761dd71a6aeed3855e15f0
WilmerGelves <125094214+WilmerGelves@users.noreply.github.com>
2026-04-08 19:34:38 -0500

    Condiciones de inicio sin registro y montaje de screen Perfil

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -2,15 +2,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../../screens/home/HomeScreen";
 import AgendaScreen from "../../screens/Agenda/AgendaScreen";
 import CalmaScreen from "../../screens/calma/CalmaScreen";
+import WelcomeScreen from "../../screens/auth/WelcomeScreen";
+import ProfileScreen from "../../screens/profile/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={WelcomeScreen} />
       <Stack.Screen name="Inicio" component={HomeScreen} />
       <Stack.Screen name="Agenda" component={AgendaScreen} />
       <Stack.Screen name="Calma" component={CalmaScreen} />
+      <Stack.Screen name="Perfil" component={ProfileScreen} />
     </Stack.Navigator>
   );
 }
